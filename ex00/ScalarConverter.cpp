50b03d088f91761c8b43e28202e35b319fc273df
Anouar El maaroufi <ael-maar@student.1337.ma>
2023-10-19 13:28:30 +0100

    Handling the conversion of the char to other scalart types

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: ael-maar <ael-maar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 11:10:15 by ael-maar          #+#    #+#             */
-/*   Updated: 2023/10/18 21:52:01 by ael-maar         ###   ########.fr       */
+/*   Updated: 2023/10/19 13:17:33 by ael-maar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ void ScalarConverter::convert(const std::string str_literal) {
     type.is_numerical = true;
     type.value = strtod(str_literal.c_str(), &end);
     type.end_type = end;
-    if (type.end_type.length() > 1 || type.end_type == str_literal)
+    if (type.end_type.length() > 1)
         type.is_numerical = false;
     convertToChar(type, str_literal);
-    convertToInt(type);
-    convertToFloat(type);
-    convertToDouble(type);
+    convertToInt(type, str_literal);
+    convertToFloat(type, str_literal);
+    convertToDouble(type, str_literal);
 }
 
 // Convert to char
@@ -40,20 +40,24 @@ void ScalarConverter::convertToChar(t_type_convert &type, const std::string &str
 }
 
 // Convert to int
-void ScalarConverter::convertToInt(t_type_convert &type) {
+void ScalarConverter::convertToInt(t_type_convert &type, const std::string &str_literal) {
     if (type.value > INT_MAX || type.value < INT_MIN || !type.is_numerical 
     || std::isinf(type.value) || std::isnan(type.value))
         std::cout << "int: Impossible" << std::endl;
+    else if (type.end_type == str_literal)
+        std::cout << "int: " << static_cast<int>(type.end_type[0]) << std::endl;
     else
         std::cout << "int: " << static_cast<int>(type.value) << std::endl;
 }
 
 // Convert to float
-void ScalarConverter::convertToFloat(t_type_convert &type) {
+void ScalarConverter::convertToFloat(t_type_convert &type, const std::string &str_literal) {
     if ((std::isinf(type.value) || std::isnan(type.value)) && type.is_numerical)
         std::cout << "float: " << type.value << "f" << std::endl;
     else if (!type.is_numerical)
         std::cout << "float: Impossible" << std::endl;
+    else if (type.end_type == str_literal)
+        std::cout << "float: " << static_cast<float>(type.end_type[0]) << ".0f" << std::endl;
     else if (static_cast<float>(type.value) == static_cast<int>(type.value))
         std::cout << "float: " << static_cast<float>(type.value) << ".0f" << std::endl;
     else
@@ -61,11 +65,13 @@ void ScalarConverter::convertToFloat(t_type_convert &type) {
 }
 
 // Convert to double
-void ScalarConverter::convertToDouble(t_type_convert &type) {
+void ScalarConverter::convertToDouble(t_type_convert &type, const std::string &str_literal) {
     if ((std::isinf(type.value) || std::isnan(type.value)) && type.is_numerical)
         std::cout << "double: " << type.value << std::endl;
     else if (!type.is_numerical)
         std::cout << "double: Impossible" << std::endl;
+    else if (type.end_type == str_literal)
+        std::cout << "double: " << static_cast<double>(type.end_type[0]) << ".0" << std::endl;
     else if (static_cast<double>(type.value) == static_cast<int>(type.value))
         std::cout << "double: " << static_cast<double>(type.value) << ".0" << std::endl;
     else
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: ael-maar <ael-maar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 11:10:17 by ael-maar          #+#    #+#             */
-/*   Updated: 2023/10/17 19:04:02 by ael-maar         ###   ########.fr       */
+/*   Updated: 2023/10/19 13:16:42 by ael-maar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,9 @@ typedef struct s_type_convert {
 class ScalarConverter
 {
     static void convertToChar(t_type_convert &type, const std::string &str_literal);
-    static void convertToInt(t_type_convert &type);
-    static void convertToFloat(t_type_convert &type);
-    static void convertToDouble(t_type_convert &type);
+    static void convertToInt(t_type_convert &type, const std::string &str_literal);
+    static void convertToFloat(t_type_convert &type, const std::string &str_literal);
+    static void convertToDouble(t_type_convert &type, const std::string &str_literal);
     public:
         static void convert(const std::string str_literal);
 };
