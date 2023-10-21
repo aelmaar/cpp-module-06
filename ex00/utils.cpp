0ae5c1e056655805f0e9068886b3bee70a8fb935
Anouar El maaroufi <ael-maar@student.1337.ma>
2023-10-21 14:20:38 +0100

    Fixed some issues when printing the number after the decimal point
    Setting the floating point number to fixed point notation, so that the numbers are printed with a fixed digits after the decimal point

---
+++ ex00/utils.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-maar <ael-maar@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/21 14:00:06 by ael-maar          #+#    #+#             */
+/*   Updated: 2023/10/21 14:12:56 by ael-maar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "utils.hpp"
+
+int calc_precision_len(const std::string &str_literal)
+{
+    int precision_len;
+    size_t locate_f = str_literal.find("f");
+    try
+    {
+        precision_len = str_literal.substr(str_literal.find(".")).length() - 1;
+        if (locate_f != std::string::npos && precision_len > 1)
+            precision_len--;
+    }
+    catch(const std::exception& e)
+    {
+        precision_len = 1;
+    }
+    return (precision_len);
+}
---
+++ ex00/utils.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-maar <ael-maar@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/21 13:59:48 by ael-maar          #+#    #+#             */
+/*   Updated: 2023/10/21 14:01:24 by ael-maar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UTILS_HPP
+# define UTILS_HPP
+
+# include <iostream>
+
+int calc_precision_len(const std::string &str_literal);
+
+#endif
